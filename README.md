8636dddfc240892cb4778be2c388ac9f5ddce47c
Ben <150054499+Beleebs@users.noreply.github.com>
2025-04-29 01:10:38 -0400

    Final submission

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # BankersAlgorithmOS
+
+## Project 2 for Operating Systems @ Kent State University
+Ben Leber
+Dr. Guan
+CS33211
+4/28/2025
+
+### How to run it
+
+Navigate to the main directory for the application and run the command <code>g++ banker.cpp -o banker</code>.
+This should generate the <code>banker</code> executable, which can be ran by then typing <code>./banker bankerInfo.txt</code>
--- banker.cpp
+++ banker.cpp
@@ -6,6 +6,7 @@
 std::vector<int> getNeed(std::vector<int>);
 bool isSafe(std::vector<int>&, std::vector<int>, std::vector<int>);
 void adjustAvailable(std::vector<int>&, std::vector<int>);
+bool checkStates(std::vector<bool>);
 
 int main(int argc, char* argv[]) {
 
@@ -13,6 +14,8 @@ int main(int argc, char* argv[]) {
 	std::vector<int> p0, p1, p2, p3, p4;
 	// Available resources
 	std::vector<int> available;
+	// Safe sequence
+	std::vector<int> safeSequence;
 
 	// Number is the variable that holds a number taken from the file
 	int number = 0;
@@ -22,10 +25,9 @@ int main(int argc, char* argv[]) {
 	// takes the text file for input
 	// std::fstream filein(argv[0]);
 	// for testing purposes, im gonna just take banker.txt locally
-	std::ifstream filein("bankerInfo.txt");
+	std::ifstream filein(argv[1]);
 	if (filein.is_open()) {
-		std::cout << "Opened file :)" << std::endl;
-		
+
 		// takes the integer input
 		while (filein >> number) {
 
@@ -82,6 +84,80 @@ int main(int argc, char* argv[]) {
 		std::vector<int> need3 = getNeed(p3);
 		std::vector<int> need4 = getNeed(p4);
 
+		// contains the safe states
+		std::vector<bool> states = { false, false, false, false, false };
+
+		// Determines safe state sequence
+		// goes 5 times, max number of iterations possible with 5 processes
+		for (int i = 0; i < 10; i++) {
+			// checks for if the process is able to be checked for safe state
+			// if it already is (meaning states[x] == true), skip
+			// P0
+			if (states[0] == false) {
+				// checks if safe
+				if (isSafe(available, need0, p0)) {
+					// pushes true for the process
+					// adds the process number to the safe sequence
+					states[0] = true;
+					safeSequence.push_back(0);
+				}
+				else
+					states[0] = false;
+			}
+			// P1
+			if (states[1] == false) {
+				if (isSafe(available, need1, p1)) {
+					states[1] = true;
+					safeSequence.push_back(1);
+				}
+				else
+					states[1] = false;
+			}
+			// P2
+			if (states[2] == false) {
+				if (isSafe(available, need2, p2)) {
+					states[2] = true;
+					safeSequence.push_back(2);
+				}
+				else
+					states[2] = false;
+			}
+			// P3
+			if (states[3] == false) {
+				if (isSafe(available, need3, p3)) {
+					states[3] = true;
+					safeSequence.push_back(3);
+				}
+				else
+					states[3] = false;
+			}
+			// P4
+			if (states[4] == false) {
+				if (isSafe(available, need4, p4)) {
+					states[4] = true;
+					safeSequence.push_back(4);
+				}
+				else
+					states[4] = false;
+			}
+			if (checkStates(states)) {
+				break;
+			}
+		}
+
+		if (!checkStates(states)) {
+			std::cout << "The system is not in a safe state." << std::endl;
+		}
+		else {
+			std::cout << "The system is in a safe state!" << std::endl;
+			std::cout << "The safe state is: ";
+			// loop for outputting safe state
+			for (int i = 0; i < safeSequence.size(); i++) {
+				std::cout << safeSequence[i] << " ";
+			}
+            std::cout << std::endl;
+		}
+
 		filein.close();
 	}
 	else {
@@ -89,6 +165,7 @@ int main(int argc, char* argv[]) {
 	}
 }
 
+// returns the need for a process
 std::vector<int> getNeed(std::vector<int> input) {
 	// return variable
 	std::vector<int> result;
@@ -102,6 +179,8 @@ std::vector<int> getNeed(std::vector<int> input) {
 	return result;
 }
 
+// Checks to see if the available resources are able to provide for the need
+// if it is, send the available resources to adjustAvailable() with allocated resources
 bool isSafe(std::vector<int>& avail, std::vector<int> need, std::vector<int> allocation) {
 	// avail = [#, #, #]
 	// need  = [#, #, #]
@@ -119,9 +198,19 @@ bool isSafe(std::vector<int>& avail, std::vector<int> need, std::vector<int> all
 	return true;
 }
 
+// Adjusts the amount available based on allocated amount
 void adjustAvailable(std::vector<int>& avail, std::vector<int> allocation) {
 	for (int i = 0; i < avail.size(); i++) {
 		avail[i] += allocation[i];
 	}
+}
+
+// checks to see if all the states are valid for a state sequence
+bool checkStates(std::vector<bool> states) {
+	for (int i = 0; i < states.size(); i++) {
+		if (states[i] == false)
+			return false;
+	}
+	return true;
 }
 
